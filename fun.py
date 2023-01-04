370559cb856be237ec8395080054bab75d52133a
c4t0212 <winter.coldwu@gmail.com>
2023-01-05 00:24:39 +0800

    downgrade to 13.7

--- fun.py
+++ fun.py
@@ -13,7 +13,7 @@ from pytube import YouTube as Youtube
 import openai
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def start(update: Update, context: CallbackContext):
     msg = \
     """
     使用方法:
@@ -21,11 +21,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     IG貼文圖片抓取: /ig [IG網址]
     YT MP3下載: /yt [YT網址]
     """
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
-async def get_img(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def get_img(update: Update, context: CallbackContext):
     query = update.callback_query
-    await query.edit_message_text(text=f"生成中...", reply_markup=None)
+    query.edit_message_text(text=f"生成中...", reply_markup=None)
     imgNums, text = query.data.split(' ', 1)
 
     headers = {
@@ -40,34 +40,34 @@ async def get_img(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     res = json.loads(req.post('https://api.openai.com/v1/images/generations', headers=headers, json=body).text)
     for url in res['data']:
-        await context.bot.send_photo(update.callback_query.message.chat.id, photo=url['url'])
+        context.bot.send_photo(update.callback_query.message.chat.id, photo=url['url'])
 
-    await context.bot.send_message(chat_id=update.callback_query.message.chat.id, text='生成完畢 uwu',reply_markup=InlineKeyboardMarkup([[
+    context.bot.send_message(chat_id=update.callback_query.message.chat.id, text='生成完畢 uwu',reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton("重新生成", callback_data=query.data)]]))
 
-async def img(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def img(update: Update, context: CallbackContext):
     text = update.message.text[4:]
     test = 'cute animate girl' if text == '' else text
 
     MAXN = 5
-    await update.message.reply_text('選擇要生成幾張圖片', reply_markup=InlineKeyboardMarkup([[
+    update.message.reply_text('選擇要生成幾張圖片', reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton(str(x), callback_data=f'{x}'+text) for x in range(1, MAXN+1)
     ]]))
     return get_img
 
-async def yt(update: Update, context: CallbackContext):
+def yt(update: Update, context: CallbackContext):
     url = update.message.text[4:]
     if url == '':
-        await context.bot.send_message(chat_id=update.message.chat_id, text='請貼上連結 >w<')
+        context.bot.send_message(chat_id=update.message.chat_id, text='請貼上連結 >w<')
         return
-    msg = await context.bot.send_message(chat_id=update.message.chat_id, text='下載中...')
+    msg = context.bot.send_message(chat_id=update.message.chat_id, text='下載中...')
     yt = Youtube(url)
     # yt = Youtube(url,on_progress_callback=on_progress)
     file = yt.streams.filter().get_audio_only().title + '.mp3'
     yt.streams.filter(only_audio=True).first().download(filename=file)
-    msg = await context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='下載完畢')
-    msg = await context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='上傳中...')
-    await context.bot.send_audio(chat_id=update.message.chat.id, audio=open(file, 'rb'), write_timeout=1000, read_timeout=1000)
-    msg = await context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='上傳完畢')
+    msg = context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='下載完畢')
+    msg = context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='上傳中...')
+    context.bot.send_audio(chat_id=update.message.chat.id, audio=open(file, 'rb'), timeout=1000)
+    msg = context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text='上傳完畢')
     print(file)
     os.remove(file)
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import openai
 from fun import *
 from telegram import *
+from telegram.ext import *
 from dotenv import load_dotenv
 
 class TelegramBot():
     def __init__(self):
         load_dotenv()
         token = os.getenv('TOKEN')
-        self.app = ApplicationBuilder().token(token).build()
+        self.app = Updater(token=token, use_context=True, base_url='http://0.0.0.0:8080/bot')
+        # self.app = Updater(token=token, use_context=True)
         self.CommandHandlerName = ['start', 'img', 'yt']
         self.CallbackQueryHandlerName = ['get_img']
     
     def run(self):
         for fun in self.CommandHandlerName:
-            self.app.add_handler(CommandHandler(fun, eval(fun)))
+            self.app.dispatcher.add_handler(CommandHandler(fun, eval(fun)))
         for fun in self.CallbackQueryHandlerName:
-            self.app.add_handler(CallbackQueryHandler(eval(fun)))
-
+            self.app.dispatcher.add_handler(CallbackQueryHandler(eval(fun)))
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-        self.app.run_polling()
+        self.app.start_polling()
+        self.app.idle()
 
 if __name__ == '__main__':
     bot = TelegramBot()
+    # bot.app.bot.logOut()
     bot.run()
