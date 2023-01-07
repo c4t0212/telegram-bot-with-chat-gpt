2535ba43881c0305b758efa0fc18422dded42a22
c4t0212 <winter.coldwu@gmail.com>
2023-01-07 16:36:06 +0800

    change inline keyboard content

--- fun.py
+++ fun.py
@@ -43,15 +43,15 @@ class TelegramBotFunction():
         }
 
         res = json.loads(req.post('https://api.openai.com/v1/images/generations', headers=headers, json=body).text)
-        for url in res['data']:
-            context.bot.send_photo(update.callback_query.message.chat.id, photo=url['url'])
-
+        context.bot.send_media_group(update.callback_query.message.chat.id, media=[InputMediaPhoto(media=url['url']) for url in res['data']])
+        context.dispatcher.remove_handler(CallbackQueryHandler(self.get_img))
         context.bot.send_message(chat_id=update.callback_query.message.chat.id, text='生成完畢 uwu',reply_markup=InlineKeyboardMarkup([[
             InlineKeyboardButton("重新生成", callback_data=query.data)]]))
 
     def img(self, update: Update, context: CallbackContext):
         self.args = 'img'
         context.bot.send_message(chat_id=update.message.chat_id, text='請輸入文字 uwu')
+        context.dispatcher.add_handler(CallbackQueryHandler(self.get_img))
         context.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.get_text))
 
     def get_text(self, update: Update, context: CallbackContext):
@@ -59,11 +59,9 @@ class TelegramBotFunction():
         text = update.message.text
 
         if self.args == 'img':
-            MAXN = 5
             self.args = text
-            update.message.reply_text('選擇要生成幾張圖片', reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton(str(x), callback_data=x) for x in range(1, MAXN+1)
-            ]], one_time_keyboard=True))
+            inlineKeyboard = [[InlineKeyboardButton(str(_ * 4 + x), callback_data=_ * 4 + x) for x in range(1, 5)] for _ in range(2)]
+            update.message.reply_text('選擇要生成幾張圖片', reply_markup=InlineKeyboardMarkup(inlineKeyboard, one_time_keyboard=True))
             return self.get_img
         elif self.args[:2] == 'yt':
             if self.args == 'ytv':
@@ -71,10 +69,12 @@ class TelegramBotFunction():
             else:
                 self.args = text
             return self.get_yt(update, context)
+        return CommandHandler.end
     
     def get_res(self, update: Update, context: CallbackContext):
         query = update.callback_query
         query.edit_message_text(text=f"下載中...")
+        context.dispatcher.remove_handler(CallbackQueryHandler(self.get_res))
         resolution = query.data + 'p'
         yt = self.args
         file = yt.streams.filter(res=resolution).first().title + '.mp4'
@@ -92,8 +92,6 @@ class TelegramBotFunction():
         file = ''
         if self.args[0] == '-':
             resolution = [int(i.split("p")[0]) for i in (list(dict.fromkeys([i.resolution for i in yt.streams if i.resolution])))]
-            # resolution = [i for i in (list(dict.fromkeys([i.resolution for i in yt.streams if i.resolution])))]
-            # file = resolution.title + '.' + resolution.subtype
             resolution.sort()
             self.args = yt
             msg = context.bot.edit_message_text(chat_id=update.message.chat_id, message_id=msg.message_id, text=f'選擇要下載的畫質\n', reply_markup=InlineKeyboardMarkup([[
@@ -117,6 +115,7 @@ class TelegramBotFunction():
     def yt_video(self, update: Update, context: CallbackContext):
         self.args = 'ytv'
         context.bot.send_message(chat_id=update.message.chat_id, text='請貼上連結 >w<')
+        context.dispatcher.add_handler(CallbackQueryHandler(self.get_yt))
         context.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.get_text))
 
     def err(self, update: Update, context: CallbackContext):
--- main.py
+++ main.py
@@ -17,10 +17,9 @@ class TelegramBot():
     def run(self):
         for fun in self.CommandHandlerName:
             self.app.dispatcher.add_handler(CommandHandler(fun, eval('self.func.' + fun)))
-        for fun in self.CallbackQueryHandlerName:
-            self.app.dispatcher.add_handler(CallbackQueryHandler(eval('self.func.' + fun)))
-        # self.app.dispatcher.add_handler(ErrorHandler(error, error_callback))
-        # self.app.dispatcher.add_error_handler(self.func.err)
+        # for fun in self.CallbackQueryHandlerName:
+        #     self.app.dispatcher.add_handler(CallbackQueryHandler(eval('self.func.' + fun)))
+        self.app.dispatcher.add_error_handler(self.func.err)
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
         self.app.start_polling()
         self.app.idle()
